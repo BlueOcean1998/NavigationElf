9b7efca43106b5e6ef63f7f7c95ef4da4c3f8cd2
BlueEra <2872545042@qq.com>
2020-03-25 19:54:06 +0800

    new search UI

--- app/src/main/java/com/example/foxizz/navigation/MainActivity.java
+++ app/src/main/java/com/example/foxizz/navigation/MainActivity.java
@@ -2,9 +2,20 @@ package com.example.foxizz.navigation;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.ValueAnimator;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
@@ -18,15 +29,19 @@ import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.UiSettings;
 import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
     private BaiduMap mBaiduMap;
+    private UiSettings mUiSettings;
+
+    private MyOrientationListener myOrientationListener;//方向传感器
+    private float mLastX;//方向角度
 
     private LocationClient mLocationClient;
-
     private boolean isFirstLoc = true;//是否是首次定位
     private MyLocationData locData;//地址信息
     private LatLng latLng;//坐标
@@ -35,8 +50,48 @@ public class MainActivity extends AppCompatActivity {
     private double mLatitude;//纬度
     private double mLongitude;//经度
 
-    private MyOrientationListener myOrientationListener;//方向传感器
-    private float mLastX;//方向角度
+    private LinearLayout searchLayout;//搜索布局
+    private EditText searchEdit;//搜索输入框
+    private Button searchButton1;//清除按钮
+    private Button searchButton2;//搜索按钮
+    private ImageButton searchExpand;//搜索结果伸缩按钮
+    private LinearLayout searchDrawer;//搜索结果抽屉
+    private RecyclerView searchResult;//搜索结果列表
+
+    private static boolean expandFlag = false;//伸缩状态
+    private int bodyHeight;//半个屏幕高度
+
+    //获取改变控件尺寸动画
+    //参数：需要改变高度的layoutDrawer（当然也可以是其它view），动画前的高度，动画后的高度
+    private ValueAnimator getValueAnimator(final View view, int startHeight, int endHeight) {
+        final ValueAnimator valueAnimator = ValueAnimator.ofInt(startHeight, endHeight);
+        //valueAnimator.setDuration(300);//动画时间（默认就是300）
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //逐渐改变view的高度
+                view.getLayoutParams().height = (int) animation.getAnimatedValue();
+                view.requestLayout();
+            }
+        });
+        return valueAnimator;
+    }
+
+    //伸展按钮的旋转动画
+    //参数：需要旋转的spreadButton（当然也可以是其它view），动画前的旋转角度，动画后的旋转角度
+    private void rotateExpandIcon(final View view, float from, float to) {
+        final ValueAnimator valueAnimator = ValueAnimator.ofFloat(from, to);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());//先加速后减速的动画
+        //valueAnimator.setDuration(300);//动画时间（默认就是300）
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //逐渐改变view的旋转角度
+                view.setRotation((float) valueAnimator.getAnimatedValue());
+            }
+        });
+        valueAnimator.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //初始化地图控件
         setInit();
+        //初始化自定义控件
+        initMyView();
         //初始化方向传感器
         initMyOrien();
         //初始化定位
@@ -55,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
         //获取地图控件引用
         mMapView = findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
+        mUiSettings = mBaiduMap.getUiSettings();
 
         //配置定位图层显示方式，使用默认的定位图标，设置精确度圆的填充色和边框色
         //LocationMode定位模式有三种：普通模式，跟随模式，罗盘模式，在这使用普通模式
@@ -64,6 +122,65 @@ public class MainActivity extends AppCompatActivity {
         mBaiduMap.setMyLocationConfiguration(myLocationConfiguration);
 
         mMapView.removeViewAt(1);//去除百度水印
+        mMapView.showScaleControl(true);//显示比例尺
+        mMapView.showZoomControls(false);//去除缩放按钮
+        mUiSettings.setCompassEnabled(false);//去除指南针
+    }
+
+    //初始化自定义控件
+    private void initMyView() {
+        searchLayout = findViewById(R.id.search_layout);
+        searchEdit = findViewById(R.id.search_edit);
+        searchButton1 = findViewById(R.id.search_button1);
+        searchButton2 = findViewById(R.id.search_button2);
+        searchExpand = findViewById(R.id.search_expand);
+        searchDrawer = findViewById(R.id.search_drawer);
+        searchResult = findViewById(R.id.search_result);
+
+        searchButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchEdit.setText("");
+            }
+        });
+
+        searchButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchContent = searchEdit.getText().toString();
+                if(!searchContent.equals("")){
+
+                }
+            }
+        });
+
+        //计算半个屏幕高度，用于下面的伸缩动画
+        Display defaultDisplay = getWindowManager().getDefaultDisplay();
+        Point point = new Point();
+        defaultDisplay.getSize(point);
+        bodyHeight = point.y / 2;
+
+        searchExpand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ValueAnimator valueAnimator;//伸展动画
+
+                if (expandFlag) {//如果状态为展开
+                    searchResult.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.adapter_alpha1));//动画1，消失;
+                    valueAnimator = getValueAnimator(searchDrawer, bodyHeight, 0);//设置抽屉动画为收起
+                    rotateExpandIcon(searchExpand, 180, 0);//伸展按钮的旋转动画
+                    expandFlag = false;//设置状态为收起
+
+                } else {//如果状态为收起
+                    searchResult.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.adapter_alpha2));//动画2，出现;
+                    valueAnimator = getValueAnimator(searchDrawer, 0, bodyHeight);//设置抽屉动画为展开
+                    rotateExpandIcon(searchExpand, 0, 180);//伸展按钮的旋转动画
+                    expandFlag = true;//设置状态为展开
+                }
+
+                valueAnimator.start();//开始抽屉的伸缩动画
+            }
+        });
     }
 
     //初始化方向传感器
@@ -128,17 +245,23 @@ public class MainActivity extends AppCompatActivity {
                     mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
 
                     if(mLocType == BDLocation.TypeGpsLocation) {//GPS定位结果
-                        Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,
+                                location.getAddrStr(), Toast.LENGTH_SHORT).show();
                     } else if(mLocType == BDLocation.TypeNetWorkLocation) {//网络定位结果
-                        Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,
+                                location.getAddrStr(), Toast.LENGTH_SHORT).show();
                     } else if(mLocType == BDLocation.TypeOffLineLocation) {//离线定位结果
-                        Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,
+                                location.getAddrStr(), Toast.LENGTH_SHORT).show();
                     } else if(mLocType == BDLocation.TypeServerError) {//服务器错误
-                        Toast.makeText(MainActivity.this, "服务器错误，请检查", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,
+                                "服务器错误，请检查", Toast.LENGTH_SHORT).show();
                     } else if(mLocType == BDLocation.TypeNetWorkException) {//网络错误
-                        Toast.makeText(MainActivity.this, "网络错误，请检查", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,
+                                "网络错误，请检查", Toast.LENGTH_SHORT).show();
                     } else if(mLocType == BDLocation.TypeCriteriaException) {//手机模式错误
-                        Toast.makeText(MainActivity.this, "手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,
+                                "手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -184,6 +307,8 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient.start();
     }
 
+
+
     //管理地图的生命周期
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -220,4 +345,5 @@ public class MainActivity extends AppCompatActivity {
         }
         mMapView.onDestroy();
     }
+
 }
